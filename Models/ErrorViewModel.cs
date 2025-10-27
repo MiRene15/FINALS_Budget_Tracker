0c959a85662957716b3f795c3a787c53041b650a
Lorene_Valencia <ladymirene@proton.me>
2025-10-28 02:17:52 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FINALS_Budget_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
