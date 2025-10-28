91e7797941f6a2fbf7838fe1ec14c2c89902d9c9
Lorene_Valencia <ladymirene@proton.me>
2025-10-28 12:19:08 +0800

    Brief fix and update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using FINALS_Budget_Tracker.Infrastructure;
 using FINALS_Budget_Tracker.Infrastructure.Entities;
 using FINALS_Budget_Tracker.Services;
